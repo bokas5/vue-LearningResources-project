8792fccb7ff0a64b9b41616119375c993f165262
bokas5 <bojanraden@gmail.com>
2021-01-19 18:35:26 +0100

    adding stored resources and add resources buttons

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,21 @@
 <template>
   <the-header :title="thetitle"></the-header>
-  <stored-resources :resources="storedResources"></stored-resources>
+  <the-resources></the-resources>
 </template>
 <script>
-import StoredResources from './components/learning-resources/StoredResources.vue';
+
 import TheHeader from './components/layouts/TheHeader.vue';
+import TheResources from './components/learning-resources/TheResources.vue';
+
 
 export default {
-  components: { StoredResources, TheHeader },
+  components: {TheHeader,TheResources },
   data() {
-    return {
+    return{
+   
       thetitle: 'Practice',
 
-      storedResources: [
-        {
-          id: 'official-guide',
-          title: 'Official Guide',
-          description: 'The official Vue.js documentation',
-          link: 'https://vuejs.org'
-        },
-        {
-          id: 'Google',
-          title: 'Google',
-          description: 'Learn to google..',
-          link: 'https://google.org'
-        }
-      ]
+      
     };
   }
 };
