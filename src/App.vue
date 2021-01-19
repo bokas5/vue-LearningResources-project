3738f4f1cedf869cec43aeee951d8a60ab870272
bokas5 <bojanraden@gmail.com>
2021-01-19 17:25:43 +0100

    styling and more components

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,12 @@
-<template>
-  <ul>
-    <learning-resources
-      v-for="res in storedResources"
-      :key="res.id"
-      :title="res.title"
-      :description="res.description"
-      :link="res.link"
-    ></learning-resources>
-  </ul>
-</template>
 
+<template>
+  <stored-resources :resources="storedResources"></stored-resources>
+</template>
 <script>
-import LearningResources from './components/learning-resources/LearningResource.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
+
 export default {
-  components: { LearningResources },
+  components: { StoredResources },
   data() {
     return {
       storedResources: [
@@ -35,4 +28,18 @@ export default {
 };
 </script>
 
-<style></style>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Roboto', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
