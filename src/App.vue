812843c91dd98de714665cdcee295371ad927e3c
bokas5 <bojanraden@gmail.com>
2021-01-19 17:06:42 +0100

    components & props

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <ul>
+    <learning-resources
+      v-for="res in storedResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resources>
+  </ul>
+</template>
+
+<script>
+import LearningResources from './components/learning-resources/LearningResource.vue';
+export default {
+  components: { LearningResources },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'Google',
+          title: 'Google',
+          description: 'Learn to google..',
+          link: 'https://google.org'
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style></style>
